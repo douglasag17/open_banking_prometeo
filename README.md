84e9bb734cfdc61ca57d8a8f61886f9d0deb141f
Douglas Ardila Garcés <dag1702@hotmail.com>
2025-01-28 11:11:20 -0500

    commiting to not lost these changes

--- README.md
+++ README.md
@@ -19,3 +19,49 @@ python3 main.py
 
 deactivate
 ```
+
+
+
+
+Create API
+- Calls Prometeo's API and writes to a propetary db
+- A Frontend app will make calls to retrieve data from propetary db (not prometeo)
+- user should only be able to
+  -  change the category of the transaction
+  -  change frequency of the transaction -> recurrent (one-time, daily, weekly, monthly, every 3 months, yearly, etc)
+  -  change note
+- user shouldn't be able to add a transaction
+- Frontend -> Streamlit
+  - https://retool.com/integrations/rest-api
+  - React ?
+- Transactional DB
+  - account
+    - id_account
+    - number
+    - name
+    - type (savings, checking, credit card, investment, loan, property, asset)
+    - is_connected (T/F) -> automatic with an open banking solution like prometeo
+    - currency
+    - balance
+    - due_date (credit card, loan)
+    - raw (json)
+  - transaction
+    - id_transaction
+    - id_account
+    - date
+    - amount (expense -> negative | income -> positive)
+    - reference
+    - detail
+    - extra_data
+    - notes
+    - tag
+    - subcategory_id
+  - category
+    - category_id
+    - name
+    - type (needs, wants, savings)
+  - subcategory
+    - subcategory_id
+    - category_id
+    - name
+    - type (needs, wants, savings)
--- main.py
+++ main.py
@@ -9,19 +9,17 @@ class PrometeoAuth:
     def __init__(self, api_key: str, base_url: str) -> None:
         self.api_key = api_key
         self.base_url = base_url
-        self.session_key = self.get_session_key()
+        self.session_key = ""
 
-    def login(self) -> Dict:
+    def login(self, provider: str, username: str, password: str) -> str:
         url = f"{self.base_url}/login/"
-        data = {"provider": "test", "username": "12345", "password": "gfdsa"}
+        data = {"provider": provider, "username": username, "password": password}
         headers = {"X-API-Key": self.api_key}
         response = requests.post(url, data=data, headers=headers)
         if response.ok:
-            return response.json()
-
-    def get_session_key(self) -> str:
-        login_data: Dict = self.login()
-        return login_data.get("key")
+            response_json = response.json()
+            self.session_key = response_json.get("key", "")
+        return self.session_key
 
     def logout(self) -> bool:
         url = f"{self.base_url}/logout/"
@@ -36,6 +34,14 @@ class PrometeoData:
         self.api_key = api_key
         self.base_url = base_url
         self.session_key = session_key
+
+    def get_providers(self) -> List[Dict]:
+        """Get the providers info from the API"""
+        url = f"{self.base_url}/provider/"
+        headers = {"X-API-Key": self.api_key}
+        response = requests.get(url, headers=headers)
+        if response.ok:
+            return response.json().get("providers", [])
 
     def get_info_user(self) -> Dict:
         url = f"{self.base_url}/info/"
@@ -53,7 +59,7 @@ class PrometeoData:
         if response.ok:
             return response.json().get("accounts", [])
 
-    def get_account_movements(
+    def get_account_transactions(
         self,
         account_number: str,
         currency: str,
@@ -72,7 +78,7 @@ class PrometeoData:
         response = requests.get(url, params=params, headers=headers)
         if response.ok:
             return response.json().get("movements", [])
-    
+
     def get_credit_cards(self) -> List[Dict]:
         url = f"{self.base_url}/credit-card/"
         params = {"key": self.session_key}
@@ -81,7 +87,7 @@ class PrometeoData:
         if response.ok:
             return response.json().get("credit_cards", [])
 
-    def get_credit_card_movements(
+    def get_credit_card_transactions(
         self,
         card_number: str,
         currency: str,
@@ -107,57 +113,55 @@ def main():
     api_key = os.getenv("API_KEY")
     base_url = "https://banking.sandbox.prometeoapi.com"
     prometeo_auth = PrometeoAuth(api_key=api_key, base_url=base_url)
+    prometeo_auth.login(provider="test", username="12345", password="gfdsa")
     prometeo_data = PrometeoData(
         api_key=api_key, base_url=base_url, session_key=prometeo_auth.session_key
     )
     start_date = datetime.now().replace(day=1, month=5)
     end_date = datetime.now()
-    
+
     info_user: Dict = prometeo_data.get_info_user()
     print("info_user:", info_user)
-    
+
     accounts: List[Dict] = prometeo_data.get_accounts()
     print("accounts:", accounts)
 
     credit_cards: List[Dict] = prometeo_data.get_credit_cards()
     print("credit_cards:", credit_cards)
 
-    movements_data = {}
+    transactions_data = {}
     for account in accounts:
         account_number = account.get("number")
-        movements_data[account_number] = prometeo_data.get_account_movements(
-            account_number,
-            account.get("currency", "USD"),
-            start_date,
-            end_date
+        transactions_data[account_number] = prometeo_data.get_account_transactions(
+            account_number, account.get("currency", "USD"), start_date, end_date
         )
     for credit_card in credit_cards:
         card_number = credit_card.get("number")
-        movements_data[card_number] = prometeo_data.get_credit_card_movements(
-            card_number,
-            credit_card.get("currency", "USD"),
-            start_date,
-            end_date
+        transactions_data[card_number] = prometeo_data.get_credit_card_transactions(
+            card_number, credit_card.get("currency", "USD"), start_date, end_date
         )
 
     accounts_expenses = {}
     accounts_credits = {}
     accounts_balance = {}
-    for account_number, movements in movements_data.items():
-        for movement in movements:
-            if debit := movement.get("debit"):
+    for account_number, transactions in transactions_data.items():
+        for transaction in transactions:
+            if debit := transaction.get("debit"):
                 accounts_expenses[account_number] = (
                     accounts_expenses.get(account_number, 0) + debit
                 )
-            if credit := movement.get("credit"):
+            if credit := transaction.get("credit"):
                 accounts_credits[account_number] = (
                     accounts_credits.get(account_number, 0) + credit
                 )
-        accounts_balance[account_number] = accounts_credits[account_number] - accounts_expenses[account_number]
+        accounts_balance[account_number] = (
+            accounts_credits[account_number] - accounts_expenses[account_number]
+        )
     print("accounts_expenses", accounts_expenses)
     print("accounts_credits", accounts_credits)
     print("accounts_balance", accounts_balance)
     prometeo_auth.logout()
+
 
 if __name__ == "__main__":
     main()
